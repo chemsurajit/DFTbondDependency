bb9c53f76073fd071f093407f1b4d491dfc4e9d7
surajit_n <surajit.nandi1988@gmail.com>
2022-05-24 21:37:00 +0200

    minor

--- make_reactions_parallel.py
+++ make_reactions_parallel.py
@@ -123,8 +123,8 @@ def proces_reaction_data_column(rids_pd, coreno, nodeno, molecule_data_pd, g4mp2
         pdt_g4mp2_ens = g4mp2_en.loc[pdt_indices, "index"]
         logging.info("No of react g4mp2 energy rows: %d. pid: %d" % (react_g4mp2_ens.shape[0], pid))
         logging.info("No of pdt g4mp2 energy rows: %d. pid: %d" % (pdt_g4mp2_ens.shape[0], pid))
-        mol_react_data = molecule_data_pd.loc[molecule_data_pd["index"].isin(react_indices)]
-        mol_pdt_data = molecule_data_pd.loc[molecule_data_pd["index"].isin(pdt_indices)]
+        mol_react_data = molecule_data_pd.loc[react_indices, "index"]
+        mol_pdt_data = molecule_data_pd.loc[pdt_indices, "index"]
         logging.debug("number of rows in reactant df: %d. pid: %d" % (mol_react_data.shape[0], pid))
         logging.debug("number of rows in pdt df: %d. pid: %d" % (mol_pdt_data.shape[0], pid))
         logging.info("Now dataframe wise subtraction will be performed pid: %d" % pid)
