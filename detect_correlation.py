9302d524e754ad6e96d27f8b17ba404fcb3da9ab
surna <surna@dtu.dk>
2022-05-31 13:40:12 +0200

    minor

--- detect_correlation.py
+++ detect_correlation.py
@@ -2,18 +2,30 @@ import os, glob, sys
 import argparse
 import pandas as pd
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
 
 
-bonds_list = ['C_s_C', 'C_d_C', 'C_t_C', 'C_C_A', 'C_s_H', 'C_s_O',
+#bonds_list = ['C_s_C', 'C_d_C', 'C_t_C', 'C_C_A', 'C_s_H', 'C_s_O',
+#              'C_d_O', 'C_O_A', 'C_s_N', 'C_d_N', 'C_t_N', 'C_N_A',
+#              'O_s_H', 'O_s_N', 'O_d_N', 'O_N_A', 'N_s_N', 'N_d_N',
+#              'N_N_A', 'N_s_H']
+
+#remove the bonds with H
+#bonds_list = ['C_s_C', 'C_d_C', 'C_t_C', 'C_C_A', 'C_s_O',
+#              'C_d_O', 'C_O_A', 'C_s_N', 'C_d_N', 'C_t_N', 'C_N_A',
+#              'O_s_N', 'O_d_N', 'O_N_A', 'N_s_N', 'N_d_N',
+#              'N_N_A']
+
+
+bonds_list = ['C_d_C', 'C_t_C', 'C_C_A', 'C_s_O',
               'C_d_O', 'C_O_A', 'C_s_N', 'C_d_N', 'C_t_N', 'C_N_A',
-              'O_s_H', 'O_s_N', 'O_d_N', 'O_N_A', 'N_s_N', 'N_d_N',
-              'N_N_A', 'N_s_H']
+              'O_s_N', 'O_d_N', 'O_N_A', 'N_s_N', 'N_d_N',
+              'N_N_A']
 
-
-def detect_correlation(csv_files, dft_functional, output):
+def detect_correlation(csv_files, output, frac=0.1):
     val_x = []
     val_y = []
-    total_df = pd.DataFrame()
+    df_collect = []
     for csvf in csv_files:
         nchunk = 0
         print("Reading csv file: ", csvf)
@@ -21,13 +33,23 @@ def detect_correlation(csv_files, dft_functional, output):
             nchunk += 1
             df = chunk.dropna(axis=0, how='any')
             df = df.loc[(df[bonds_list].abs().sum(axis=1) != 0)]
-            df["dE"] = df.loc[:, ("G4MP2")] - df.loc[:, (dft_functional.upper())]
-            total_df = total_df.append(df.sample(frac=0.10), ignore_index=True)
-    val_bonds = total_df[bonds_list]
+            if frac == 1.0:
+                df_collect.append(df)
+            else:
+                df_collect.append(df.sample(frac=frac))
+            if (nchunk%10) == 0:
+                print("Nchunk:", nchunk)
+    total_df = pd.concat(df_collect, ignore_index=True)
+    print("Dataset reading complete. Proceeding for correlation check with ", frac, " of data")
+    print("ndf row: ", total_df.shape[0])
+    X = add_constant(total_df[bonds_list])
+    # to return the result to a dataframe:
     vif_data = pd.DataFrame()
-    vif_data["feature"] = bonds_list
-    vif_data["vif"] = [variance_inflation_factor(val_bonds.values, i) for i in range(len(val_bonds.columns))]
+    vif_data["feature"] = X.columns
+    vif_data["VIF"] = pd.Series(variance_inflation_factor(X.values, i) for i in range(X.shape[1]))
     print(vif_data)
+    vif_data.to_csv(output)
+    return
 
 
 
@@ -46,17 +68,20 @@ def main():
     parser = argparse.ArgumentParser("Script to perform linear regression for different DFT functionals.")
     parser.add_argument('-d', '--data_dir', help="Path where the Reactions_n.csv files are kept. \
                         default, current directory.", default="./")
-
-    parser.add_argument('-f', '--dft_functional', required=True, help="Name of the dft functional \
-                        from which to calculate errors in reaction energies. Required only if --limit is used.")
+    parser.add_argument('-f', '--frac_data', help="Fraction of data to be used to calculate the correlation. Value allowed: 0<frac<1.0.", default=0.5)
 
     args = parser.parse_args()
     csv_dir = args.data_dir
     csv_files = get_csv_files(csv_dir)
-    dft_functional = args.dft_functional
+    frac = float(args.frac_data)
     output = "correlation.csv"
     if not output.endswith(".csv"):
         print("Only csv files allowed as output data file")
         parser.print_help()
         sys.exit()
-    detect_correlation(csv_files, dft_functional, output)
+    detect_correlation(csv_files, output, frac=frac)
+    return
+
+
+if __name__ == "__main__":
+    main()
